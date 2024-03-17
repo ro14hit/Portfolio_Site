456e6c544c6d856f7b53c051ad582567ef6b4488
ro14hit <rohit1499singh@gmail.com>
2024-03-17 23:27:41 +0530

    Added header section

---
+++ lib/data.ts
@@ -0,0 +1,34 @@
+export const links = [
+    {
+      name: "Home",
+      hash: "#home",
+    },
+    {
+      name: "About",
+      hash: "#about",
+    },
+    {
+      name: "Projects",
+      hash: "#projects",
+    },
+    {
+      name: "Skills",
+      hash: "#skills",
+    },
+    {
+      name: "Experience",
+      hash: "#experience",
+    },
+    {
+        name: "Certifications",
+        hash: "#certifications",
+      },
+    {
+        name: "Education",
+        hash: "#education",
+      },
+    {
+      name: "Contact",
+      hash: "#contact",
+    },
+  ] as const;
